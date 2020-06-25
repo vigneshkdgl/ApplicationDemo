dc3d26f5cf09b3f769e86b9679b609b817351311
yokesh-7095 <yokeshwaran.k@zohocorp.com>
2020-06-25 06:36:55 +0530

    First Commit

---
+++ Applicationdemo/src/main/java/com/vignesh/Applicationdemo/ApplicationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.vignesh.Applicationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationdemoApplication.class, args);
+	}
+
+}
